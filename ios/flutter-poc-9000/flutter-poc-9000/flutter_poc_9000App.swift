36507638c78dcea48eb76d197bee6e7e2fcbaaeb
Dávid Maróthi <david.marothi@hotmail.com>
2024-09-13 21:11:25 +0200

    feat: add empty iOS app

---
+++ ios/flutter-poc-9000/flutter-poc-9000/flutter_poc_9000App.swift
@@ -0,0 +1,17 @@
+//
+//  flutter_poc_9000App.swift
+//  flutter-poc-9000
+//
+//  Created by Dávid Maróthi on 2024-09-13.
+//
+
+import SwiftUI
+
+@main
+struct flutter_poc_9000App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
