c8fd0a6a4fa5e3aa2a9afd90bc51f941dea71fee
Dávid Maróthi <david.marothi@hotmail.com>
2024-09-14 01:20:43 +0200

    feat: add flutter module

--- ios/flutter-poc-9000/flutter-poc-9000/ContentView.swift
+++ ios/flutter-poc-9000/flutter-poc-9000/ContentView.swift
@@ -6,14 +6,32 @@
 //
 
 import SwiftUI
+import Flutter
+
+struct FlutterViewControllerRepresentable: UIViewControllerRepresentable {
+  @Environment(FlutterDependencies.self) var flutterDependencies
+
+  func makeUIViewController(context: Context) -> some UIViewController {
+    return FlutterViewController(
+      engine: flutterDependencies.flutterEngine,
+      nibName: nil,
+      bundle: nil)
+  }
+
+  func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+}
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        NavigationStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            NavigationLink("Next") {
+                FlutterViewControllerRepresentable()
+            }
+            .buttonStyle(.borderedProminent)
         }
         .padding()
     }
--- ios/flutter-poc-9000/flutter-poc-9000/flutter_poc_9000App.swift
+++ ios/flutter-poc-9000/flutter-poc-9000/flutter_poc_9000App.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
+import Flutter
+
+@Observable
+class FlutterDependencies {
+    let flutterEngine = FlutterEngine(name: "my flutter engine")
+    init() {
+        // Runs the default Dart entrypoint with a default Flutter route.
+        flutterEngine.run()
+    }
+}
 
 @main
 struct flutter_poc_9000App: App {
+    @State var flutterDependencies = FlutterDependencies()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(flutterDependencies)
         }
     }
 }
